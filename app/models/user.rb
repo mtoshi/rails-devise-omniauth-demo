daf5f8884c017fbffff2a857879d4f8120f9d478
mtoshi <mtoshi@outlook.com>
2020-05-25 13:24:53 +0900

    Change user model for OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    User.where(email: data['email']).first
+  end
+
 end
