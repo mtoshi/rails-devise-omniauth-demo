f74cb2b5154dde19d7cfc537330e31c15974fd51
mtoshi <mtoshi@outlook.com>
2020-06-04 16:25:56 +0900

    Add home auth require

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
+  authenticate :user do
+    root to: 'home#index', as: :authenticated_root
+  end
+
+  root to: redirect('/users/sign_in')
+
 end
